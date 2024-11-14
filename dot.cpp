7759acf415d846fda6169b2a64c699249fc89e91
Shubhrati <2023ucp1624@mnit.ac.in>
2024-11-14 12:25:07 +0530

    Dot File Added

---
+++ dot.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b;
+     cout<<"Enter 1st vector value: ";
+    cin>>a>>b;
+    int c,d;
+    cout<<"Enter 2nd vector value: ";
+    cin>>c>>d;
+    int prod = (a*b) + (c*d);
+    cout<<"Dot product of "<<a<<"+ i"<<b<<" and "<<c<<"+ i"<<d<<" = "<<prod<<endl;
+}
